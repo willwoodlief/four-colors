11697c2ce7262a33f3a2329ce24e73c44d822442
Will Woodlief <willwoodlief@gmail.com>
2022-12-18 03:30:21 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta lang="en">
+        <title>
+            Baby Momma HexBatch
+        </title>
+        <meta name="description" content="Stub For Static Pages">
+        <meta name="author" content="Will Woodlief">
+
+        <link rel="stylesheet" href="resources/css/hexbatch.css">
+
+        <link rel="shortcut icon" type="image/png" href="images/favicon-16x16.png"/>
+
+
+    </head>
+
+    <body>
+        <section>
+            <div class="hex-parent">
+                <div class="hexbatch">
+                    <div class="hexagon">
+                        <div class="spoiler">
+                            Creating 
+                            <strong>
+                                <em>
+                                    real
+                                </em>
+                            </strong> 
+                            websites ..
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <footer>
+                <div class="afterthought">
+                    Yep!!
+                </div>
+                            
+            </footer>
+        </section>
+        
+    </body>
+</html>
